74b7f7d367cc688be64b09f38e7c9ba507bb861e
Sebastian Seijo <sebastiangseijo@gmail.com>
2021-04-04 23:11:05 +0200

    Add initial commands and first impl

--- dist/bot.js
+++ dist/bot.js
@@ -4,3 +4,7 @@ const dotenv_1 = require("dotenv");
 dotenv_1.config();
 const discord_js_1 = require("discord.js");
 const client = new discord_js_1.Client();
+client.login(process.env.DISCORD_TOKEN);
+client.on('ready', () => {
+    console.log('Bot is Ready');
+});
--- src/bot.ts
+++ src/bot.ts
@@ -2,6 +2,9 @@ import {config} from 'dotenv';
 config();
 
 import { Client } from 'discord.js';
+const client: Client = new Client();
 
-
-const client: Client = new Client()
+client.login(process.env.DISCORD_TOKEN);
+client.on('ready', () => {
+    console.log('Bot is Ready');
+});
