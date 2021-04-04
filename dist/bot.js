d808bdd561cbc2868f4d991840d8c8ee4c458ec2
Sebastian Seijo <sebastiangseijo@gmail.com>
2021-04-04 22:56:04 +0200

    Add dist folder to ignore

--- dist/bot.js
+++ dist/bot.js
@@ -3,5 +3,4 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const dotenv_1 = require("dotenv");
 dotenv_1.config();
 const discord_js_1 = require("discord.js");
-process.env.DISCORD_TOKEN;
 const client = new discord_js_1.Client();
