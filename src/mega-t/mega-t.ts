1ce8272815c7ebb43105351990125b704e98c5f9
Sebastian Seijo <sebastiangseijo@gmail.com>
2021-04-05 19:43:05 +0200

    Remove Scrapping mechanism due to not working as expected

--- src/mega-t/mega-t.ts
+++ src/mega-t/mega-t.ts
@@ -1,6 +1,5 @@
 import { Message, MessageEmbed } from "discord.js";
 import { JUSTSWAP_URL_MEGA_T } from "../config.json";
-import puppeteer from "puppeteer";
 import { MegaResultData } from "./interfaces";
 
 export const price = async (message: Message) => {
@@ -10,36 +9,10 @@ export const price = async (message: Message) => {
 
 const buildMessage = async () => {
   const embed: MessageEmbed = new MessageEmbed();
-  const result: MegaResultData = await navigateAndCollectData();
   embed.setTitle(`JustSwap`);
-  embed.setURL(`${JUSTSWAP_URL_MEGA_T}`);
-  embed.addFields(
-    { name: "Mega Price", value: `${result.megaPrice}` },
-    { name: "TRX Price", value: `1 TRX ${result.trxPrice}` }
+  embed.setDescription(
+    `Unfortunately, Mega-T Token has not been listed yet. Until then, please visit JustSwap to get more information`
   );
+  embed.setURL(`${JUSTSWAP_URL_MEGA_T}`);
   return embed;
-};
-
-const navigateAndCollectData = async () => {
-  const browser = await puppeteer.launch({
-    args: ["--no-sandbox", "--disable-setuid-sandbox"],
-  });
-  const page = await browser.newPage();
-  await page.goto(`${JUSTSWAP_URL_MEGA_T}`);
-  // wait page to load data
-  await page.waitForTimeout(3000);
-  const result: MegaResultData = await page.evaluate(() => {
-    // Get Just Swap div with price elements
-    const data = document.querySelector(".pr-price")?.textContent;
-    // split data-price in two.
-    const splittedData = data?.split("1TRX");
-    const megaPrice = splittedData![0];
-    const trxPrice = splittedData![1];
-    return {
-      megaPrice,
-      trxPrice,
-    };
-  });
-  return result;
-  browser.close();
 };
