5d788436b70abdafe44ed9576407accd42052697
Sebastian Seijo <sebastiangseijo@gmail.com>
2021-04-05 17:33:05 +0200

    Add parameters to puppeteer

--- src/mega-t/mega-t.ts
+++ src/mega-t/mega-t.ts
@@ -21,7 +21,9 @@ const buildMessage = async () => {
 };
 
 const navigateAndCollectData = async () => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    args: ["--no-sandbox", "--disable-setuid-sandbox"],
+  });
   const page = await browser.newPage();
   await page.goto(`${JUSTSWAP_URL_MEGA_T}`);
   // wait page to load data
